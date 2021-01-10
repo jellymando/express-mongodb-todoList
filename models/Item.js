40aaf26a0cd442dc60b8b00c0d69e6fcbd4eccc3
dain <cicikdi@naver.com>
2021-01-10 18:34:09 +0900

    feat: new Schama

--- models/Item.js
+++ models/Item.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
 
-const itemSchema = mongoose.Schema({
+const itemSchema = new Schema({
   title: {
       type: String,
       required: true
